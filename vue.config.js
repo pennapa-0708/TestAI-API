dc6b05d54235b90bd10b1556143fe111c15738d5
ploy <pennapa3280@gmail.com>
2023-05-28 22:15:42 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule("sass")
+      .test(/\.sass$/)
+      .use("sass-loader")
+      .loader("sass-loader")
+      .end();
+  },
+};
