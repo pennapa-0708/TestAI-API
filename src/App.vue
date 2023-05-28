0145e008f1655d91b67e96a385970ff2e42456b7
ploy <pennapa3280@gmail.com>
2023-05-28 22:04:00 +0700

    Add UI and fuction validateSubmit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h2>Question & Answer</h2>
+      <div class="box">
+        <div>
+          <div class="title">
+            <p>Question</p>
+          </div>
+          <div>
+            <input class="input-question" v-model="question" />
+          </div>
+          <div class="mt-4">
+            <button class="button" @click="validateAndSubmit()">Submit</button>
+          </div>
+        </div>
+        <hr />
+        <div class="mt-4">
+          <div class="title">
+            <p>Answer</p>
+          </div>
+          <div class="box-answer">
+            <div>
+              <p>Test Answer</p>
+              <P></P>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import aiService from "./service/aiService";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
+  components: {},
+  data() {
+    return {
+      question: "",
+      answer: "",
+      isValidInput: true,
+      lim: 1,
+      minscore: 0.1,
+      url_base: "https://api.aiforthai.in.th/ptt-qsearch/predict?text=",
+    };
+  },
+  created() {},
+  computed: {},
+  methods: {
+    fetch_question() {
+      fetch(`${this.url_base}${this.question}&lim=${this.lim}&minscore=${this.minscore}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then(this.setResults);
+    },
+    setResults(result) {
+      this.answer = result;
+    },
 
-<style>
+    DoGetquestionSearch() {
+      aiService.GetQuestionSearch(this.question);
+    },
+    validateInput(input) {
+      const regex = /^[a-zA-Z0-9ก-๙เแอ-ฮ]+$/;
+      return regex.test(input);
+    },
+    validateAndSubmit() {
+      this.isValidInput = this.validateInput(this.question);
+      if (this.isValidInput) {
+        this.fetch_question();
+      } else {
+        alert("กรุณากรอกข้อความอีกครั้งโดยที่ไม่มีอักขระพิเศษ");
+      }
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +87,37 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .question {
+    display: flex;
+    justify-content: space-around;
+  }
+  .box {
+    border: 1px solid;
+    padding: 20px;
+    margin: 50px;
+  }
+  .title {
+    font-weight: bold;
+  }
+  .button {
+    background-color: #4caf50; /* Green */
+    border: none;
+    color: white;
+    padding: 5px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+  }
+  .input-question {
+    width: 800px;
+  }
+  .box-answer {
+    border: #4caf50 solid 1px;
+    padding: 20px;
+    margin: 10px 490px 10px 490px;
+    height: max-content;
+    text-align: start;
+  }
 }
 </style>
